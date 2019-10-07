f0220eaf9f65a0fd5a682bb7974f1aa5f4973249
Özgün Özdemir <ozgunozdemir@ucuracak.com>
2019-10-07 12:08:09 +0300

    review table component

--- src/components/Ui/Table/Table.js
+++ src/components/Ui/Table/Table.js
@@ -1,72 +1,63 @@
-import React from 'react'
-import './Table.css'
+import React, { Component } from "react";
+import "./Table.css";
 
-const Table = () => {
+import { getStory } from "../../../services/index";
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allStory: null
+    };
+  }
+
+  componentDidMount = async () => {
+    setInterval(async() => {
+      const result = await getStory();
+      if (result.status === "success") {
+        this.setState({
+          allStory: result.data
+        });
+      } else {
+        alert("Story veritabanından çekilemedi.");
+      }
+    }, 2000);
+  
+  };
+
+  render() {
     return (
-        <div className="table-container">
-            <table>
-              <thead>
-                <tr>
-                  <th>Story</th>
-                  <th>Story Point</th>
-                  <th>Status</th>
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Story</th>
+              <th>Story Point</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.allStory ? (
+              this.state.allStory.map((item, i) => 
+                <tr key={i}>
+                  <td>Story {i + 1}</td>
+                  <td>{item.finalScore ? item.finalScore : ""}</td>
+                  <td>{item.status}</td>
                 </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Story 1</td>
-                  <td></td>
-                  <td>Active</td>
-                </tr>
-                <tr>
-                  <td>Story 2</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 3</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 4</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 5</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 6</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 7</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 8</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 9</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-                <tr>
-                  <td>Story 10</td>
-                  <td></td>
-                  <td>Not Voted</td>
-                </tr>
-              </tbody>
-            </table>
-            </div>
+              )
+            ) : (
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     );
+  }
 }
 
-export { Table }
+export { Table };
